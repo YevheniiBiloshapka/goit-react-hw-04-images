e5318d0fd566d573811a36362421413e04f134b8
YevheniiBiloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2022-10-18 21:34:37 +0300

    fix even listener

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,28 +1,28 @@
 import PropTypes from 'prop-types';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalImg } from './Modal.styled';
+import { useEffect } from 'react';
 
 const modalRoot = document.querySelector('#modal-root');
 
 function Modal({ hits, onClose }) {
    const { tags, largeImageURL } = hits;
-   window.addEventListener(`keydown`, handleKeyDown);
 
-   function handleKeyDown(e) {
-      if (e.code === 'Escape') {
+   const handleKeyDown = e => {
+      if (e.currentTarget === e.target || e.code === 'Escape') {
          onClose();
+      }
+   };
+
+   useEffect(() => {
+      window.addEventListener(`keydown`, handleKeyDown);
+      return () => {
          return window.removeEventListener(`keydown`, handleKeyDown);
-      }
-   }
-
-   function handleBackDropClick(e) {
-      if (e.currentTarget === e.target) {
-         onClose();
-      }
-   }
+      };
+   });
 
    return createPortal(
-      <Overlay onClick={handleBackDropClick}>
+      <Overlay onClick={onClose}>
          <ModalImg>
             <img src={largeImageURL} alt={tags} />
          </ModalImg>
